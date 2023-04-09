026814343191fdfe53e1a47f67d3434141d4dafb
Gerardo Napoli <gerry.nap98@gmail.com>
2023-04-10 01:37:10 +0200

    Final Website

--- js/main.js
+++ js/main.js
@@ -1,47 +1,19 @@
-$(document).ready(() => {
-    let splitted = ((window.location.href).split('#'))[1];
-
-    if([undefined, "", null].includes(splitted))
-        $(".showcase-area").load("pages/home.html");
-    else
-        $(".showcase-area").load("pages/" + splitted + ".html");
-});
-
-const main = document.querySelector("main");
-var big_wrapper = document.querySelector(".big-wrapper");
-
-let dark = false;
-let active = false;
-
-function toggleAnimation(location){
-    if(active){
-        activeMenu();
-        active = false;
-    }
-    dark = !dark;
-    let clone = big_wrapper.cloneNode(true);
-    if(dark){
-        clone.classList.remove("light");
-        clone.classList.add("dark");
-    } else {
-        clone.classList.remove("dark");
-        clone.classList.add("light");
-    }
-    clone.classList.add("copy");
-    main.appendChild(clone);
-    $(".showcase-area").eq(1).load("pages/" + location + ".html");
-
-    document.body.classList.add("stop-scrolling");
-
-    clone.addEventListener("animationend", () => {
-        document.body.classList.remove("stop-scrolling");
-        big_wrapper.remove();
-        clone.classList.remove("copy");
-        big_wrapper = document.querySelector(".big-wrapper");
-    });
-}
+var imageIndex = 1;
+setInterval(changeImage, 5000);
 
 function activeMenu(){
-    big_wrapper.classList.toggle("active");
-    active = !active;
+    document.querySelector(".big-wrapper").classList.toggle("active");
 }
+
+function goHome(){
+    document.location.href = '/';
+}
+
+function changeImage() {
+    var myImage = document.getElementById("brandImage");
+    imageIndex++;
+    if (imageIndex > 3) {
+      imageIndex = 1;
+    }
+    myImage.src = "../img/brand/"+imageIndex + ".png";
+  }
